ca941725af958c36a7b216d4498f29a4d6b8c534
Michael Galetzka <galetzka@posteo.de>
2017-03-13 09:30:31 +0100

    Fix swap implementation

--- src/main/java/org/codefx/lab/arraylist/SwapRemover.java
+++ src/main/java/org/codefx/lab/arraylist/SwapRemover.java
@@ -1,5 +1,6 @@
 package org.codefx.lab.arraylist;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SwapRemover {
@@ -7,10 +8,14 @@ public class SwapRemover {
 	// intended for ArrayList
 
 	public static <T> List<T> remove(List<T> list, int[] removeAts) {
+		Arrays.sort(removeAts);
 		int copyFrom = list.size();
-		for (int i = 0; i < removeAts.length; i++) {
+		for (int i = removeAts.length - 1; i >= 0; i--) {
 			int removeAt = removeAts[i];
 			copyFrom--;
+			if (removeAt == copyFrom) {
+				continue;
+			}
 			list.set(removeAt, list.get(copyFrom));
 		}
 		list.subList(copyFrom, list.size())
